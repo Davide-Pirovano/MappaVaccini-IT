d7393661bd638600b60fd22faf6990a6a7f38a8d
DAVIDE PIROVANO <DAVIDE.PIROVANO@issgreppi.it>
2021-05-20 10:12:40 +0200

    Inserimento della Mappa

--- MappaVacciniIT/MappaVacciniIT/MappaVacciniIT.Android/MainActivity.cs
+++ MappaVacciniIT/MappaVacciniIT/MappaVacciniIT.Android/MainActivity.cs
@@ -6,26 +6,20 @@ using Android.Runtime;
 using Android.OS;
 using Android;
 
-
 namespace MappaVacciniIT.Droid
 {
-    [Activity(Label = "MappaVacciniIT", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "MappaVacciniIT", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        const int RequestLocationId = 0;
-
-        readonly string[] LocationPermissions =
-        {
-            Manifest.Permission.AccessCoarseLocation,
-            Manifest.Permission.AccessFineLocation
-        };
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            Xamarin.FormsMaps.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
@@ -38,7 +32,7 @@ namespace MappaVacciniIT.Droid
                 }
                 else
                 {
-                    // Permissions granted - display a message.
+                    // Permissions denied - display a message.
                 }
             }
             else
@@ -62,5 +56,13 @@ namespace MappaVacciniIT.Droid
                 }
             }
         }
+
+        const int RequestLocationId = 0;
+
+        readonly string[] LocationPermissions =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+        };
     }
 }
