e23a0b35a5b34e6524c1b09220b11ecba5cbd8bb
Davide <davide.pirovano@issgreppi.it>
2021-05-20 11:57:20 +0200

    Deserializzazione Singole Regioni

---
+++ MappaVacciniIT/MappaVacciniIT/MappaVacciniIT/DeserializzazioneVaccini.cs
@@ -0,0 +1,45 @@
+﻿using System;
+public class DeserializzazioneVaccini
+{
+    public Schema schema { get; set; }
+    public Datum[] data { get; set; }
+}
+
+public class Schema
+{
+    public Field[] fields { get; set; }
+    public string[] primaryKey { get; set; }
+    public string pandas_version { get; set; }
+}
+
+public class Field
+{
+    public string name { get; set; }
+    public string type { get; set; }
+}
+
+public class Datum
+{
+    public int index { get; set; }
+    public DateTime data_somministrazione { get; set; }
+    public string area { get; set; }
+    public int totale { get; set; }
+    public int sesso_maschile { get; set; }
+    public int sesso_femminile { get; set; }
+    public int categoria_operatori_sanitari_sociosanitari { get; set; }
+    public int categoria_personale_non_sanitario { get; set; }
+    public int categoria_ospiti_rsa { get; set; }
+    public int categoria_personale_scolastico { get; set; }
+    public int categoria_60_69 { get; set; }
+    public int categoria_70_79 { get; set; }
+    public int categoria_over80 { get; set; }
+    public int categoria_soggetti_fragili { get; set; }
+    public int categoria_forze_armate { get; set; }
+    public int categoria_altro { get; set; }
+    public int prima_dose { get; set; }
+    public int seconda_dose { get; set; }
+    public string codice_NUTS1 { get; set; }
+    public string codice_NUTS2 { get; set; }
+    public int codice_regione_ISTAT { get; set; }
+    public string nome_area { get; set; }
+}
--- MappaVacciniIT/MappaVacciniIT/MappaVacciniIT/MainPage.xaml.cs
+++ MappaVacciniIT/MappaVacciniIT/MappaVacciniIT/MainPage.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -10,9 +12,131 @@ namespace MappaVacciniIT
 {
     public partial class MainPage : ContentPage
     {
+        HttpClient client = new HttpClient();
+        static string url = "https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.json";
+        string dati;
+        public Dictionary<string, int> totRegioni = new Dictionary<string, int>() {
+                {"Lombardia",0 },
+                {"Veneto",0 },
+                {"Lazio",0 },
+                {"Marche",0 },
+                {"Abruzzo",0 },
+                {"Toscana",0 },
+                {"Emilia-Romagna",0 },
+                {"Piemonte",0 },
+                {"Friuli-Venezia Giulia",0 },
+                {"Campania",0 },
+                {"Basilicata",0 },
+                {"Molise",0 },
+                {"Umbria",0 },
+                {"Sardegna",0 },
+                {"Sicilia",0 },
+                {"Puglia",0 },
+                {"Provincia Autonoma Trento",0 },
+                {"Valle d'Aosta / Vallée d'Aoste",0 },
+                {"Liguria",0 },
+                {"Calabria",0 },
+                {"Provincia Autonoma Bolzano / Bozen",0 },
+            };
         public MainPage()
         {
             InitializeComponent();
+            Get();
+            DeserializzazioneVaccini dV = JsonConvert.DeserializeObject<DeserializzazioneVaccini>(dati);
+            foreach (var item in dV.data)
+            {
+
+                if (item.nome_area == "Lombardia")
+                {
+                    totRegioni["Lombardia"] += item.totale;
+                }
+                else if (item.nome_area == "Veneto")
+                {
+                    totRegioni["Veneto"] += item.totale;
+                }
+                else if (item.nome_area == "Piemonte")
+                {
+                    totRegioni["Piemonte"] += item.totale;
+                }
+                else if (item.nome_area == "Abruzzo")
+                {
+                    totRegioni["Abruzzo"] += item.totale;
+                }
+                else if (item.nome_area == "Marche")
+                {
+                    totRegioni["Marche"] += item.totale;
+                }
+                else if (item.nome_area == "Lazio")
+                {
+                    totRegioni["Lazio"] += item.totale;
+                }
+                else if (item.nome_area == "Campania")
+                {
+                    totRegioni["Campania"] += item.totale;
+                }
+                else if (item.nome_area == "Provincia Autonoma Trento")
+                {
+                    totRegioni["Provincia Autonoma Trento"] += item.totale;
+                }
+                else if (item.nome_area == "Umbria")
+                {
+                    totRegioni["Umbria"] += item.totale;
+                }
+                else if (item.nome_area == "Emilia-Romagna")
+                {
+                    totRegioni["Emilia-Romagna"] += item.totale;
+                }
+                else if (item.nome_area == "Molise")
+                {
+                    totRegioni["Molise"] += item.totale;
+                }
+                else if (item.nome_area == "Friuli-Venezia Giulia")
+                {
+                    totRegioni["Friuli-Venezia Giulia"] += item.totale;
+                }
+                else if (item.nome_area == "Toscana")
+                {
+                    totRegioni["Toscana"] += item.totale;
+                }
+                else if (item.nome_area == "Sicilia")
+                {
+                    totRegioni["Sicilia"] += item.totale;
+                }
+                else if (item.nome_area == "Basilicata")
+                {
+                    totRegioni["Basilicata"] += item.totale;
+                }
+                else if (item.nome_area == "Puglia")
+                {
+                    totRegioni["Puglia"] += item.totale;
+                }
+                else if (item.nome_area == "Sardegna")
+                {
+                    totRegioni["Sardegna"] += item.totale;
+                }
+                else if (item.nome_area == @"Valle d'Aosta / Vallée d'Aoste")
+                {
+                    totRegioni["Valle d'Aosta / Vallée d'Aoste"] += item.totale;
+                }
+                else if (item.nome_area == @"Liguria")
+                {
+                    totRegioni["Liguria"] += item.totale;
+                }
+                else if (item.nome_area == @"Calabria")
+                {
+                    totRegioni["Calabria"] += item.totale;
+                }
+                else if (item.nome_area == @"Provincia Autonoma Bolzano / Bozen")
+                {
+                    totRegioni["Provincia Autonoma Bolzano / Bozen"] += item.totale;
+                }
+            }
+
+        }
+
+        async void Get()
+        {
+            dati = await client.GetStringAsync(url);
         }
     }
 }
